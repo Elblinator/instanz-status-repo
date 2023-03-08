0d4f1503095731c6873c8ddc1cf14a65514b83f9
Loki Elble <Loki Elble lelble@intevation.de>
2023-03-08 08:23:56 +0100

    different pages now hidden if logged out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+import { UserService } from './user.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(
+    private userService: UserService
+  ) {}
+
+  public isLoggedIn(): boolean{
+    return this.userService.isLoggedIn()
+  }
+
   title = 'Instanzen';
 }
