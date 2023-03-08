1ab3f5d95ef39536d14d3ae58a1217c0a4178918
Loki Elble <Loki Elble lelble@intevation.de>
2023-03-08 09:40:03 +0100

    plan.md changed

---
+++ src/app/filter.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+import { StatusService } from './status.service';
+import { Instanz } from './00_data/instanzen';
+import { Status } from './00_data/status';
+@Injectable({
+  providedIn: 'root'
+})
+export class FilterService {
+
+  constructor() { }
+}
