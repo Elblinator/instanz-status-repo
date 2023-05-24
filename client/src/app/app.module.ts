00f1d47cf6576fe8be34d0adfd771197fd7f6cd4
Loki Elble <lelble@intevation.de>
2023-05-24 15:37:03 +0200

    organised imports

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,81 +1,79 @@
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatNativeDateModule } from '@angular/material/core';
+import { HttpClientModule } from '@angular/common/http';
+import { LayoutModule } from '@angular/cdk/layout';
+import { NgModule } from '@angular/core';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatSelectModule } from '@angular/material/select';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatInputModule } from '@angular/material/input';
-import { MatRadioGroup, MatRadioModule } from '@angular/material/radio';
-
-
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { HttpClientModule } from '@angular/common/http';
-import { TranslateModule, TranslatePipe } from '@ngx-translate/core';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatRadioGroup, MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { TranslateModule, TranslatePipe } from '@ngx-translate/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { StartComponent } from './start/start.component';
-import { ServiceComponent } from './service/service.component';
 import { InstanceComponent } from './instance/instance.component';
 import { InstanceDetailComponent } from './instance-detail/instance-detail.component';
+import { StartComponent } from './start/start.component';
+import { ServiceComponent } from './service/service.component';
 
-import { FilterComponent } from './filter/filter-dialog.component';
-import { WarnComponent } from './warn/warn-dialog.component';
-import { GridComponent } from './grid/grid/grid.component';
-import { TileComponent } from './grid/tile/tile.component';
 import { AutocompleteComponent } from './autocomplete/autocomplete.component';
+import { FilterComponent } from './filter/filter-dialog.component';
+import { GridComponent } from './grid/grid/grid.component';
 import { GroupInfoComponent } from './GroupInfo/GroupInfo.component';
+import { TileComponent } from './grid/tile/tile.component';
+import { WarnComponent } from './warn/warn-dialog.component';
 
 
 @NgModule({
 	imports: [
+		AppRoutingModule,
+		BrowserAnimationsModule,
 		BrowserModule,
 		FormsModule,
-		AppRoutingModule,
 		HttpClientModule,
-		MatDialogModule,
-		ReactiveFormsModule,
-		MatNativeDateModule,
+		LayoutModule,
+		MatAutocompleteModule,
+		MatButtonModule,
 		MatCardModule,
 		MatCheckboxModule,
-		MatSelectModule,
-		LayoutModule,
-		MatToolbarModule,
-		MatButtonModule,
-		MatSidenavModule,
-		MatIconModule,
-		MatListModule,
-		BrowserAnimationsModule,
-		TranslateModule.forRoot(),
-		MatAutocompleteModule,
-		MatInputModule,
 		MatChipsModule,
-		MatRadioModule
+		MatDialogModule,
+		MatIconModule,
+		MatInputModule,
+		MatListModule,
+		MatNativeDateModule,
+		MatRadioModule,
+		MatSelectModule,
+		MatSidenavModule,
+		MatToolbarModule,
+		ReactiveFormsModule,
+		TranslateModule.forRoot()
 	],
 	exports: [TranslatePipe, MatRadioGroup],
 	declarations: [
 		AppComponent,
-		ServiceComponent,
+		AutocompleteComponent,
+		FilterComponent,
+		GridComponent,
+		GroupInfoComponent,
 		InstanceComponent,
 		InstanceDetailComponent,
 		StartComponent,
-		FilterComponent,
-		WarnComponent,
-		GridComponent,
+		ServiceComponent,
 		TileComponent,
-		AutocompleteComponent,
-		GroupInfoComponent
+		WarnComponent
 	],
 	bootstrap: [AppComponent,],
 	providers: [FilterComponent, ReactiveFormsModule, WarnComponent],
